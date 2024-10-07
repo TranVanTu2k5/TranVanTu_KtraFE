010d1807aa94a9ac24c112bb99ccbf570f5498fe
TU <TU@DESKTOP-M18PGH9>
2024-10-07 23:23:23 +0700

    Add project files.

---
+++ TranVanTu_KtraFE/Controllers/ShippingAddressController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TranVanTu_KtraFE.Controllers
+{
+    public class ShippingAddressController : Controller
+    {
+        // GET: ShippingAddress
+        public ActionResult ShippingAddress()
+        {
+            return View();
+        }
+    }
+}
